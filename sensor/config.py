daa8fa83181e37a6671d9d9827697c041e8aa755
Sunita778 <sunitapradhan778@gmail.com>
2023-02-11 10:59:19 +0530

    Updated requirements.txt

--- sensor/config.py
+++ sensor/config.py
@@ -2,15 +2,18 @@ import pymongo
 import pandas as pd
 import json
 from dataclasses import dataclass
-
-#Provide the mongodb localhost url to connect python to mongodb
+# Provide the mongodb localhost url to connect python to mongodb.
 import os
+
 @dataclass
-class EnviromentVAriable():
+class EnvironmentVariable:
     mongo_db_url:str = os.getenv("MONGO_DB_URL")
+    aws_access_key_id:str = os.getenv("AWS_ACCESS_KEY_ID")
+    aws_access_secret_key:str = os.getenv("AWS_SECRET_ACCESS_KEY")
 
-env_var = EnviromentVAriable()
 
-mongo_client = pymongo.MongoClient(env_car.mongo_db_url)
+env_var = EnvironmentVariable()
+mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
+TARGET_COLUMN = "class"
 
 
