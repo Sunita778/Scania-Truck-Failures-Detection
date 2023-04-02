eb1ad2a4b5951083eeaa0f12ceeda6ab6d743fcc
Sunita778 <sunitapradhan778@gmail.com>
2023-04-02 22:47:53 +0530

    updated

--- data_dump.py
+++ data_dump.py
@@ -2,23 +2,20 @@ import pymongo
 import pandas as pd
 import json
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+from sensor.config import mongo_client
 
-DATA_FILE_PATH = "/config/workspace/aps_failure_training_set1.csv"
-DATABASE_NAME = "aps"
-COLLECTION_NAME = "sensor"
-
+DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
+DATABASE_NAME="aps"
+COLLECTION_NAME="sensor"
 
 if __name__=="__main__":
     df = pd.read_csv(DATA_FILE_PATH)
-    print(f'Rows and Columns: {df.shape}')
+    print(f"Rows and columns: {df.shape}")
 
-    # Convert Dataframe to JSON so that we can dum these record in mongodb
-    df.reset_index(drop=True, inplace= True)
+    #Convert dataframe to json so that we can dump these record in mongo db
+    df.reset_index(drop=True,inplace=True)
 
     json_record = list(json.loads(df.T.to_json()).values())
     print(json_record[0])
-
-    #Insert converted json record to mongodb
-    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+    #insert converted json record to mongo db
+    mongo_client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
